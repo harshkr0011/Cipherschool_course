ff30791f4ce649784882161b0f41fe823d819827
Harsh kumar <149315734+harshkr0011@users.noreply.github.com>
2025-07-13 08:22:00 +0530

    Create App.js
    Creates a React app with a Welcome component accepting a name prop and a Box component for centering.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Welcome from './Welcome';
+import Box from './Box';
+
+function App() {
+  return (
+    <div>
+      <Box>
+        <Welcome name="Cipher Schools" />
+      </Box>
+    </div>
+  );
+}
+export default App;
